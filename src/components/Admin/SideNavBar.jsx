434a321131116efb04d1f54b8cc5e16dbb2efa3d
VenkatLohithDasari <dennytrade3@gmail.com>
2023-07-29 12:07:33 +0530

    make admin side navbar responsive

---
+++ src/components/Admin/SideNavBar.jsx
@@ -0,0 +1,76 @@
+"use client"
+import { useState, useEffect, useRef } from 'react'
+
+import websiteLogo from "../../../public/assets/images/sage stream logo.png";
+import Image from 'next/image';
+
+import SideNavBarMenu from './SideNavBarMenu'
+
+const SideNavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <>
+            <div className="w-64 min-h-screen bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-neutral-800 dark:border-gray-700 hidden 2xl:block">
+                <aside className="px-5 py-8 overflow-y-auto">
+                    <a href="#">
+                        <Image
+                            className="w-auto h-7"
+                            src={websiteLogo}
+                            alt="Website logo"
+                        />
+                    </a>
+                    <div className="flex flex-col justify-between flex-1 mt-6">
+                        <SideNavBarMenu />
+                    </div>
+                </aside>
+            </div>
+            <div className="relative">
+                <div className={`${isOpen ? "w-64 absolute" : "w-12"} min-h-screen bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-neutral-800 dark:border-gray-700 2xl:hidden transition-all ease-in-out duration-150`} ref={menuRef}>
+                    <aside className={`${isOpen ? "overflow-y-auto px-5 py-8" : ""}`}>
+                        <label className={`daisy-swap daisy-swap-rotate ${isOpen ? "" : "px-3 py-8"}`}>
+                            {/* this hidden checkbox controls the state */}
+                            <input type="checkbox" checked={isOpen} onClick={toggleMenu} />
+                            {/* hamburger icon */}
+                            <svg className="daisy-swap-off fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg>
+                            {/* close icon */}
+                            <svg className="daisy-swap-on fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" /></svg>
+                        </label>
+                        <div className={`${isOpen ? "block" : "hidden"}`}>
+                            <a href="#">
+                                <Image
+                                    className="w-auto h-7"
+                                    src={websiteLogo}
+                                    alt="Website logo"
+                                />
+                            </a>
+                            <div className="flex flex-col justify-between flex-1 mt-6">
+                                <SideNavBarMenu />
+                            </div>
+                        </div>
+                    </aside>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SideNavBar
---
+++ src/components/Admin/SideNavBarMenu.jsx
@@ -0,0 +1,148 @@
+import React from 'react'
+
+import SideBarCategory from './SideBarCategory'
+import SideBarMenuBtn from './SideBarMenuBtn'
+import { HomeModernIcon, ChartBarIcon, FilmIcon, PlusCircleIcon, TagIcon, ChatBubbleBottomCenterIcon, StarIcon, UsersIcon, UserPlusIcon, HomeIcon, GlobeEuropeAfricaIcon, PlayCircleIcon, Cog8ToothIcon, Cog6ToothIcon, LinkIcon, ArchiveBoxIcon, ArchiveBoxArrowDownIcon, ShieldCheckIcon, LockClosedIcon, ExclamationCircleIcon, WrenchScrewdriverIcon, TrashIcon, BellIcon, ArrowPathIcon } from '@heroicons/react/24/outline'
+
+const SideNavBarMenu = () => {
+    return (
+        <nav className="-mx-3 space-y-6 ">
+            <SideBarCategory title="Dashboard">
+                <SideBarMenuBtn href="/admin/dashboard">
+                    <HomeModernIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Overview</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/analytics">
+                    <ChartBarIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Analytics</span>
+                </SideBarMenuBtn>
+            </SideBarCategory>
+
+            <SideBarCategory title="Anime Management">
+                <SideBarMenuBtn href="/admin/anime">
+                    <FilmIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">All Anime</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/anime/add">
+                    <PlusCircleIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Add New Anime</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/genres">
+                    <TagIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Genres</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/comments">
+                    <ChatBubbleBottomCenterIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Comments</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/ratings-reviews">
+                    <StarIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Ratings & Reviews</span>
+                </SideBarMenuBtn>
+            </SideBarCategory>
+
+            <SideBarCategory title="Episode Management">
+                <SideBarMenuBtn href="/admin/episodes">
+                    <FilmIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">All Episodes</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/episodes/add">
+                    <PlusCircleIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Add New Episode</span>
+                </SideBarMenuBtn>
+            </SideBarCategory>
+
+            <SideBarCategory title="User Management">
+                <SideBarMenuBtn href="/admin/users">
+                    <UsersIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">All Users</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/users/add">
+                    <UserPlusIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Add New User</span>
+                </SideBarMenuBtn>
+            </SideBarCategory>
+
+            <SideBarCategory title="Site Content">
+                <SideBarMenuBtn href="/admin/home-page">
+                    <HomeIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Home Page</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/landing-page">
+                    <GlobeEuropeAfricaIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Landing Page</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/player-page">
+                    <PlayCircleIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Player Page</span>
+                </SideBarMenuBtn>
+            </SideBarCategory>
+
+            <SideBarCategory title="Settings">
+                <SideBarMenuBtn href="/admin/settings/general">
+                    <Cog8ToothIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">General Settings</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/settings/seo">
+                    <Cog6ToothIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">SEO Settings</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/settings/social-media">
+                    <LinkIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Social Media Links</span>
+                </SideBarMenuBtn>
+            </SideBarCategory>
+
+            <SideBarCategory title="Backup & Restore">
+                <SideBarMenuBtn href="/admin/backup/create">
+                    <ArchiveBoxIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Create Backup</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/backup/restore">
+                    <ArchiveBoxArrowDownIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Restore from Backup</span>
+                </SideBarMenuBtn>
+            </SideBarCategory>
+
+            <SideBarCategory title="Security">
+                <SideBarMenuBtn href="/admin/security/admin-users">
+                    <ShieldCheckIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Admin Users</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/security/roles-permissions">
+                    <LockClosedIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Roles & Permissions</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/security/logs">
+                    <ExclamationCircleIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Security Logs</span>
+                </SideBarMenuBtn>
+            </SideBarCategory>
+
+            <SideBarCategory title="Maintenance">
+                <SideBarMenuBtn href="/admin/maintenance/mode">
+                    <WrenchScrewdriverIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Maintenance Mode</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/maintenance/cleanup">
+                    <TrashIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Database Cleanup</span>
+                </SideBarMenuBtn>
+            </SideBarCategory>
+
+            <SideBarCategory title="Update & Notifications">
+                <SideBarMenuBtn href="/admin/notifications/admin">
+                    <BellIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Admin Notifications</span>
+                </SideBarMenuBtn>
+                <SideBarMenuBtn href="/admin/notifications/update">
+                    <ArrowPathIcon className="w-5 h-5" />
+                    <span className="mx-2 text-sm font-medium">Update Notifications</span>
+                </SideBarMenuBtn>
+            </SideBarCategory>
+
+        </nav>
+    )
+}
+
+export default SideNavBarMenu
